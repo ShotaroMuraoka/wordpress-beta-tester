7b64dbfe39f1188d3655a4e3c3554b6479ad13a9
pm-muraoka <shotaro_muraoka@pressman.ne.jp>
2021-02-03 18:02:26 +0900

    test for 5.6betaRC

---
+++ app/wp-content/plugins/wp10-beta/wp-beta.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Beta Testing
+Description:
+Version: 1.0.0
+Author: PM Muraoka
+License: GPLv2 or later
+*/
+add_filter('wp_is_application_passwords_available', '__return_true');
